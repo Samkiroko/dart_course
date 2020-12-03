698520bac2a0445dfc641537c2c8c1e038da1e74
Samkiroko <kirokodev@gmail.com>
2020-12-03 15:11:42 +0300

    Introduction to dart funtion

---
+++ functions.dart
@@ -0,0 +1,17 @@
+/*
+ function section
+ - How to declare and use functions (pass arguments, get return value)
+ -difference between named  and positional arguments
+ -Default values and required argument + how to use them with Null Safaty
+ -Arrow notion (=>) to write more concise functions
+ -Lexical scope & inner functions
+ -Avoiding globle mutable state & pure functions
+*/
+void main() {
+  sayHi();
+}
+
+void sayHi() {
+  print('hi');
+  print('welcome');
+}
