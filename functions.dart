2eaa033dabfce426fd1e1be0d1a8c5e913ef70fa
Samkiroko <kirokodev@gmail.com>
2020-12-03 15:54:11 +0300

    Return value

--- functions.dart
+++ functions.dart
@@ -18,20 +18,29 @@
 
 /* Function arguments*/
 
-void main() {
-  const name = 'andrea';
-  const age = 36;
-  describe(name, age);
-  const name2 = 'samuel';
-  const age2 = 32;
-  describe(name2, age2);
-}
+// void main() {
+//   const name = 'andrea';
+//   const age = 36;
+//   describe(name, age);
+//   const name2 = 'samuel';
+//   const age2 = 32;
+//   describe(name2, age2);
+// }
 
-void describe(String name, int age) {
-  print("My name is $name and I'm $age years old");
-}
+// void describe(String name, int age) {
+//   print("My name is $name and I'm $age years old");
+// }
 
 /*
 Functions help us to write more maintainable code.
 change the way a function behaves without changing how we call it.
 */
+
+void main() {
+  final person = describe('Samuel', 32);
+  print(person);
+}
+
+String describe(String name, int age) {
+  return "My name is $name. I'm $age years old";
+}
