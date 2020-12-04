0c733f68bebb9e77bfe00bcf68c7fcf4537f1025
Samkiroko <kirokodev@gmail.com>
2020-12-04 14:08:35 +0300

    the where and firstWhere methods

--- function_advance.dart
+++ function_advance.dart
@@ -76,10 +76,42 @@ Generics
 /* The where and firstWhere methods*/
 /*firstWhere*/
 // Get the first item that matches a certain condition
+// void main() {
+//   const list = [1, 3, 4, 6];
+//   final even = list.where((value) => value % 2 == 0);
+//   final odd = list.where((value) => value % 2 != 0);
+//   print(odd);
+//   print(even);
+// }
+
+/*
+## Exercise: Impliment the `where` function 
+- A list of items
+- A function argument that can be used to filter items in 
+the list by evaluating  each item with a boolean condition 
+
+The function should return a new list, which contains the items
+filtered using the function argument.
+
+Use generics, so the `where` function can work on list of any type 
+
+Then, use this function to retrieve all the odd numbers from this list `[1,2,3,4]`,
+and print the result.
+
+*/
+
 void main() {
-  const list = [1, 3, 4, 6];
-  final even = list.where((value) => value % 2 == 0);
-  final odd = list.where((value) => value % 2 != 0);
+  const list = [1, 2, 4, 5, 17, 19, 49];
+  final odd = where<int>(list, (value) => value % 2 != 0);
   print(odd);
-  print(even);
+}
+
+List<T> where<T>(List<T> items, bool Function(T) f) {
+  var result = <T>[];
+  for (var item in items) {
+    if (f(item)) {
+      result.add(item);
+    }
+  }
+  return result;
 }
