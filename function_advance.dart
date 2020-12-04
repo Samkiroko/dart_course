137ef889a3b2f41b3e8c564cc23dc78500be5f6a
Samkiroko <kirokodev@gmail.com>
2020-12-04 12:54:36 +0300

    forEach method

--- function_advance.dart
+++ function_advance.dart
@@ -18,13 +18,34 @@ Generics
 //   print(sayHi('sam'));
 // }
 /*Function as first class objects*/
-typedef Greet = String Function(String);
-void main() {
-  final sayHi = (String name) => 'Hi,$name';
-  welcome(sayHi, 'Andrea');
-}
+// typedef Greet = String Function(String);
+// void main() {
+//   final sayHi = (String name) => 'Hi,$name';
+//   welcome(sayHi, 'Andrea');
+// }
 
-void welcome(String Function(String) greet, String name) {
-  print(greet(name));
-  print('welcome to the course');
+// void welcome(String Function(String) greet, String name) {
+//   print(greet(name));
+//   print('welcome to the course');
+// }
+
+/*closures*/
+
+// void main() {
+//   const muitiplier = 10;
+//   const list = [1, 2, 3];
+//   final result = list.map((x) {
+//     return x * muitiplier;
+//   });
+//   print(result);
+// }
+
+/*the foreach method*/
+
+void main() {
+  const list = [1, 3, 4];
+  list.forEach((value) => print(value));
+  for (var value in list) {
+    print(value);
+  }
 }
